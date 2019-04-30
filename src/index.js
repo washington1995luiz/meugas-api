30bde63f24186fde6423921135a2d93a2e105ba0
Washington Luiz <washington1995luiz@gmail.com>
2019-04-30 13:03:09 -0300

    Seventh commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-require('./app/controllers/index')(app);
+require('./app/controllers/store/index')(app);
+require('./app/controllers/user/index')(app);
+
 
 
 console.log('Servidor online na porta 3000');
